c42a97506aa660e6d353d8928268d3f9839d4f5b
jelenar-vtool <149575060+jelenar-vtool@users.noreply.github.com>
2024-04-02 12:24:43 +0200

    Update argparser_txt.py

--- files/argparser_txt.py
+++ files/argparser_txt.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 def parse_doc_reg(file_path):
     defines_lines = []
@@ -14,23 +15,24 @@ def parse_doc_reg(file_path):
     
     return defines_lines
 
-def write_defines_file(defines_lines, output_file):
+def write_defines_file(defines_lines, output_dir):
+    output_file = os.path.join(output_dir, "defines.txt")
     with open(output_file, 'w') as file:
         file.writelines(defines_lines)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse doc_reg.txt and create defines.txt")
     parser.add_argument("file_path", help="Path to the input file (doc_reg.txt)")
+    parser.add_argument("-o", "--output-dir", help="Output directory for defines.txt (default: current directory)", default=os.getcwd())
     args = parser.parse_args()
-
-    output_file = "defines.txt"
 
     # Parse doc_reg.txt and get defines lines
     defines_lines = parse_doc_reg(args.file_path)
 
     if defines_lines:
         # Write the defines lines to defines.txt
-        write_defines_file(defines_lines, output_file)
-        print(f"File '{output_file}' has been created successfully.")
+        write_defines_file(defines_lines, args.output_dir)
+        print(f"File 'defines.txt' has been created successfully in '{args.output_dir}'.")
     else:
         print("No valid lines found in doc_reg.txt.")
+#defines sacuvano u users(Jelena Rajkovic)
