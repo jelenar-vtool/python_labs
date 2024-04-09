1dcc29a64f609d742a49425bad9893778ad5490c
jelenar-vtool <149575060+jelenar-vtool@users.noreply.github.com>
2024-04-09 11:57:32 +0200

    Update wildcard_bins.py

--- strings/wildcard_bins.py
+++ strings/wildcard_bins.py
@@ -1,19 +1,13 @@
 def generate_wildcard_bins(num_bins):
-    """
-    Generate wildcard bins for the specified number of bins.
-    """
-    bins = []
-    for i in range(1, num_bins + 1):
-        wildcard = "?" * (8 - i) + "1" + "?" * (i - 1)
-        bins.append(f"wildcard bins {i} = {{8'b{wildcard}}};")
-    return bins
+  
+#TODO:   Generate wildcard bins for the specified number of bins.
+   
+
 
 # Number of bins
 num_bins = 8
 
-# Generate wildcard bins
-bins = generate_wildcard_bins(num_bins)
+#TODO: Generate wildcard bins
 
-# Print the wildcard bins
-for bin in bins:
-    print(bin)
+#TODO: Print the wildcard bins
+
